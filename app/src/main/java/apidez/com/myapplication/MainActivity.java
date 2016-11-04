207d6fb703a894d78f36967917cbbef5d713b6e4
Quan Vu <nongdenchet8435848@gmail.com>
2016-11-04 12:35:25 +0700

    done

--- app/src/main/java/apidez/com/myapplication/MainActivity.java
+++ app/src/main/java/apidez/com/myapplication/MainActivity.java
@@ -2,10 +2,12 @@ package apidez.com.myapplication;
 
 import android.location.Location;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.animation.BounceInterpolator;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -13,26 +15,55 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import static apidez.com.myapplication.R.id.map;
 
 public class MainActivity extends AppCompatActivity implements LocationListener,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
-    private long UPDATE_INTERVAL = 10 * 1000;
-    private long FASTEST_INTERVAL = 2000;
+    private long UPDATE_INTERVAL = 30 * 1000;
+    private long FASTEST_INTERVAL = 5 * 1000;
+    private LatLng mCurrentLatLng;
     private GoogleApiClient mGoogleApiClient;
     private LocationRequest mLocationRequest;
+    private GoogleMap mGoogleMap;
+    private SupportMapFragment mMapFragment;
+    private Marker mMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpMapIfNeeded();
         PermissionUtils.requestLocaiton(this);
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(LocationServices.API)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
+    }
+
+    protected void setUpMapIfNeeded() {
+        if (mMapFragment == null) {
+            mMapFragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(map));
+            mMapFragment.getMapAsync(new OnMapReadyCallback() {
+                @Override
+                public void onMapReady(GoogleMap map) {
+                    mGoogleMap = map;
+                    updateCamera();
+                }
+            });
+        }
     }
 
     @Override
@@ -57,9 +88,18 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
             Location mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
             if (mCurrentLocation != null) {
                 Log.d("DEBUG", "current location: " + mCurrentLocation.toString());
-                LatLng latLng = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+                mCurrentLatLng = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
+                updateCamera();
             }
             startLocationUpdates();
+        }
+    }
+
+    private void updateCamera() {
+        if (mGoogleMap != null && mCurrentLatLng != null) {
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLng(mCurrentLatLng);
+            mGoogleMap.moveCamera(cameraUpdate);
+            addMarker();
         }
     }
 
@@ -95,6 +135,54 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
                 Double.toString(location.getLatitude()) + "," +
                 Double.toString(location.getLongitude());
         Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        mCurrentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+        updateCamera();
+    }
+
+    private void addMarker() {
+        if (mMarker != null) {
+            mMarker.remove();
+            mMarker = null;
+        }
+        BitmapDescriptor defaultMarker =
+                BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN);
+        mMarker = mGoogleMap.addMarker(new MarkerOptions()
+                .position(mCurrentLatLng)
+                .title("Some title here")
+                .snippet("Some description here")
+                .icon(defaultMarker));
+        dropPinEffect(mMarker);
+    }
+
+    private void dropPinEffect(final Marker marker) {
+        // Handler allows us to repeat a code block after a specified delay
+        final android.os.Handler handler = new android.os.Handler();
+        final long start = SystemClock.uptimeMillis();
+        final long duration = 1500;
+
+        // Use the bounce interpolator
+        final android.view.animation.Interpolator interpolator =
+                new BounceInterpolator();
+
+        // Animate marker with a bounce updating its position every 15ms
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                long elapsed = SystemClock.uptimeMillis() - start;
+                // Calculate t for bounce based on elapsed time
+                float t = Math.max(
+                        1 - interpolator.getInterpolation((float) elapsed
+                                / duration), 0);
+                // Set the anchor
+                marker.setAnchor(0.5f, 1.0f + 14 * t);
+
+                if (t > 0.0) {
+                    // Post this event again 15ms from now.
+                    handler.postDelayed(this, 15);
+                } else { // done elapsing, show window
+                    marker.showInfoWindow();
+                }
+            }
+        });
     }
 }
